39605b9bf64abd901caa675710f678f88f946d60
Streltsov Ivan <principdereva@gmail.com>
2024-04-08 17:12:16 +0200

    feat: made config

---
+++ config.js
@@ -0,0 +1,6 @@
+const currentCity = 'Moscow';
+const exactInterval = 1;
+const preferredUnits = 'm';
+const preferredLanguage = 'ru';
+
+export {currentCity, exactInterval, preferredLanguage, preferredUnits};
