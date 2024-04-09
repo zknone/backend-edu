47e824dd0149ced7eb96d7b1aa02e8eb6b283f2d
Streltsov Ivan <principdereva@gmail.com>
2024-04-09 14:14:50 +0200

    feat: fetch data

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-const currentCity = 'Moscow';
 const exactInterval = 1;
 const preferredUnits = 'm';
 const preferredLanguage = 'ru';
 
-export {currentCity, exactInterval, preferredLanguage, preferredUnits};
+module.exports = { exactInterval, preferredLanguage, preferredUnits };
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const { exactInterval, preferredLanguage, preferredUnits } = require('./config.js');
+
 const yargs = require('yargs');
 const { hideBin} = require("yargs/helpers");
 
 const argv = yargs(hideBin(process.argv)).argv;
 
-const city = argv._[0];
+const chosenCity = argv._[0];
+const weatherApiKey = process.env.WEATHER_API_KEY;
 
-console.log(city);
+const url = `http://api.weatherstack.com/current?access_key=${weatherApiKey}&query=${chosenCity}&units=${preferredUnits}`;
+
+http.get(url, (res) => {
+    const {statusCode} = res;
+    if (statusCode !== 200) {
+        console.log('Status Code:', statusCode);
+        return;
+    } 
+
+    res.setEncoding('utf-8');
+    let rawData = '';
+    res.on('data', (chunk) => rawData += chunk).on('end', ()=> console.log(rawData)).on('error', (error) => console.error(error))
+})
+
+
