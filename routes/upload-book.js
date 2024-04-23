d22218593fd57ab94e52b4186a628f8f950f92ef
Streltsov Ivan <principdereva@gmail.com>
2024-04-23 12:38:30 +0200

    fix: upload book

--- routes/upload-book.js
+++ routes/upload-book.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const fileMulter = require('../middleware/file');
 
-router.post('/upload-book', 
-    fileMulter.single('book'), 
-    (req, res, next) => {
+router.post('/:id', fileMulter.single('book'), (req, res) => {
+    const { id } = req.params;
+    const store = req.app.get('store');
+
+    const idx = store.books.findIndex(el => el.id === id);
+
+    if (idx !== -1){
         if (req.file) {
-            const { path } = req.file;
-            req.bookFilePath = path;
+            const { filename } = req.file;
+            store.books[idx].fileBook = filename; 
         }
-        next();
+        res.json(store.books[idx]);
+    } else {
+        res.status(404).json('404 | страница не найдена');
     }
-)
+});
 
 module.exports = router;
