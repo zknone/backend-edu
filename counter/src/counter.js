3f4ed6268c3bb814415023450c0d077ba2e4554a
Streltsov Ivan <principdereva@gmail.com>
2024-05-02 20:32:59 +0200

    feat: made counter

--- counter/src/counter.js
+++ counter/src/counter.js
@@ -3,11 +3,25 @@ const redis = require('redis');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const REDIS_URL = process.env.REDIS_URL || 'localhost';
 
-app.get("/counter/:bookId", (req, res) => {
-    const {bookId} = req.bookId;
+const client = redis.createClient({
+    url: REDIS_URL,
+});
 
-    res.json({bookId: bookId});
+( async() => {
+    await client.connect();
+})();
+
+app.get("/counter/:bookId", async(req, res) => {
+    const {bookId} = req.params;
+
+    try {
+        const cnt = await client.incr(bookId);
+        res.json({message: bookId, cnt});
+    } catch (e) {
+        res.json({errorcode: 500, errormessage: `Redis error: ${e}`})
+    }
 })
 
 app.post("/counter/:bookId/incr", (req, res) => {
