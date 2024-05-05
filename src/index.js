d0fd7c80a1172d8b9b982cafcd385542cf586c20
Streltsov Ivan <principdereva@gmail.com>
2024-05-05 18:08:21 +0200

    fix: deleted uuid import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const indexRouter = require('./routes/index-route');
 const path = require('path');
 const booksRouter = require('./routes/books');
-const { v4: uuid } = require('uuid');
 const mongoose = require('mongoose');
 
 const app = express();
