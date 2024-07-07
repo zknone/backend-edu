7632a8b692196ca1db788d014ae2c72a752b6724
Streltsov Ivan <principdereva@gmail.com>
2024-07-07 16:38:30 +0200

    refactor: Rename BooksRepository to BookRepository and update tsconfig.json

--- src/classes/book-repository.ts
+++ src/classes/book-repository.ts
@@ -1,6 +1,6 @@
 import { IBook } from "../models/book";
 
-export abstract class BooksRepository{
+export abstract class BookRepository{
     abstract createBook(Book: IBook): Promise<IBook>;
     abstract getBook(id: string): Promise<IBook>;
     abstract getBooks(): Promise<IBook[]>;
